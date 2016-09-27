202d49d1666e312234a861065c33f02e0e872bf5
stokescd <stokescd@mcmaster.ca>
2016-09-26 20:32:57 -0400

    Integrate ACE to work with panels.

---
+++ src/Spann/Spann/ui/UIPlayGround/demoFrame.js
@@ -0,0 +1,50 @@
+define([
+  'Frame'
+], function(Frame) {
+  return function() {
+    var frame = new Frame();
+    frame.attachLeft = [
+      {
+        component: $ui.Group,
+        id: 'status',
+        size: 'half',
+        content: [
+          {
+            component: $ui.Label,
+            value: "Chris Stokes"
+          }
+        ]
+      },
+      {
+        component: $ui.List,
+        style: $ui.NavigationListItem,
+        items: [
+          {
+            name: "Editor",
+            icon: "fa-home",
+            selected: true,
+            target: 'PlayGroup/screen_playGround'
+          }
+        ],
+        onClick: function(event) {
+          var target = event.target.model.target;
+          frame.select(target);
+        }
+      }
+    ];
+
+    frame.attachTop = [
+      {
+        component: $ui.Group,
+        content: [
+          {
+            component: $ui.Label,
+            value: "Chris Stokes"
+          }
+        ]
+      }
+    ];
+
+    return frame;
+  }
+});
