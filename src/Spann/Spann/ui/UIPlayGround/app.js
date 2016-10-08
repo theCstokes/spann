4b40ee191f5ff035495072e916d23ad0cc7616b9
Christopher Stokes <c.stokes99@gmail.com>
2016-10-08 13:15:28 -0400

    updated on click for action button

--- src/Spann/Spann/ui/UIPlayGround/app.js
+++ src/Spann/Spann/ui/UIPlayGround/app.js
@@ -8,15 +8,15 @@ define([
 
     $ui.push(screen_playGround);
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", window.location.href.split('ui/')[0] + "api/v1/File");
-    xhr.responseType = 'json';
-    xhr.onreadystatechange = function () {
-        console.log("data: " + xhr.response);
-    }
-    var data = { name: "Order66", sourceCode: "for(0 to N): Kill all Jedi;" };
-    var blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
-    xhr.send(blob);
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("POST", window.location.href.split('ui/')[0] + "api/v1/File");
+    // xhr.responseType = 'json';
+    // xhr.onreadystatechange = function () {
+    //     console.log("data: " + xhr.response);
+    // }
+    // var data = { name: "Order66", sourceCode: "for(0 to N): Kill all Jedi;" };
+    // var blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+    // xhr.send(blob);
   }
   return {
     start: start
