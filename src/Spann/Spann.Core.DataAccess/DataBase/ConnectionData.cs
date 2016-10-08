bc3fb8b13ebade30d711d01e7778e97203006c2d
Christopher Stokes <c.stokes99@gmail.com>
2016-10-08 13:06:06 -0400

    Change DBConnection to point to local DB.

--- src/Spann/Spann.Core.DataAccess/DataBase/ConnectionData.cs
+++ src/Spann/Spann.Core.DataAccess/DataBase/ConnectionData.cs
@@ -8,18 +8,29 @@ namespace Spann.Core.DataAccess.DataBase
 {
     class ConnectionData
     {
-        public static readonly string HOST = "ec2-174-129-4-75.compute-1.amazonaws.com";
+        #region Huroku Connection Data.
+        //public static readonly string HOST = "ec2-174-129-4-75.compute-1.amazonaws.com";
+        //public static readonly string PORT = "5432";
+        //public static readonly string USERNAME = "qoqjfwklwttuor";
+        //public static readonly string PASSWORD = "IJortWH8BEf5NGf_hSS_IOQBQJ";
+        //public static readonly string DATABASE = "d44iestva2nsh";
+        #endregion
+
+        #region Local Connection Data.
+        public static readonly string HOST = "127.0.0.1";
         public static readonly string PORT = "5432";
-        public static readonly string USERNAME = "qoqjfwklwttuor";
-        public static readonly string PASSWORD = "IJortWH8BEf5NGf_hSS_IOQBQJ";
-        public static readonly string DATABASE = "d44iestva2nsh";
+        public static readonly string USERNAME = "postgres";
+        public static readonly string PASSWORD = "admin";
+        public static readonly string DATABASE = "Spann";
+        #endregion
 
         public static string Credentials
         {
             get
             {
-                //return String.Format("Host={0};Port={1};Username={2};Password={3};Database={4}", 
-                return String.Format("User ID={0};Password={1};Host={2};Port={3};Database={4};Pooling=true;Use SSL Stream=True;SSL Mode=Require;TrustServerCertificate=True;",
+                //return String.Format("User ID={0};Password={1};Host={2};Port={3};Database={4};Pooling=true;Use SSL Stream=True;SSL Mode=Require;TrustServerCertificate=True;",
+                //    USERNAME, PASSWORD, HOST, PORT, DATABASE);
+                return String.Format("User ID={0};Password={1};Host={2};Port={3};Database={4};",
                     USERNAME, PASSWORD, HOST, PORT, DATABASE);
             }
         }
