c491c5b734e0dbe0dd302bbceeb444161c25a52e
stokescd <stokescd@mcmaster.ca>
2016-10-08 13:06:55 -0400

    Data code to manage response format.

--- src/Spann/Spann/ui/UIPlayGround/app.js
+++ src/Spann/Spann/ui/UIPlayGround/app.js
@@ -1,35 +1,22 @@
 define([
   'PlayGroup/demoFrame',
-  'PlayGroup/screen_playGround',
-  'Encryption'
-], function(demoFrame, screen_playGround, encryption) {
+  'PlayGroup/screen_playGround'
+], function(demoFrame, screen_playGround) {
 
   function start() {
-
-    console.log('hello world -->', encryption.saltyHash('hello world'));
     $ui.frame = demoFrame;
 
     $ui.push(screen_playGround);
 
-     //add screen
-    //  $ui.frame = mainFrame;
-
-    //  $ui.push(homeScreen);
-
-    // var xhr = new XMLHttpRequest();
-    // xhr.open("POST", window.location.href.split('ui/')[0] + "api/v1/User");
-    //
-    //   //xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
-    //   //// xhr.setRequestHeader('Content-type', 'application/ecmascript');
-    //   //xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    // xhr.responseType = 'json';
-    // xhr.onreadystatechange = function () {
-    //     console.log("data: " + xhr.response);
-    // }
-    // var message = { name: "Old Ben" };
-    // var blob = new Blob([JSON.stringify(message, null, 2)], { type: 'application/json' });
-    // xhr.send(blob);
-    // xhr.send();
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", window.location.href.split('ui/')[0] + "api/v1/File");
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = function () {
+        console.log("data: " + xhr.response);
+    }
+    var data = { name: "Order66", sourceCode: "for(0 to N): Kill all Jedi;" };
+    var blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+    xhr.send(blob);
   }
   return {
     start: start
