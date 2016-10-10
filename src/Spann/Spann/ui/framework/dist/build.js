b8272fcf761f969a295deb22465a5e84de18bc24
Christopher Stokes <c.stokes99@gmail.com>
2016-10-10 12:20:05 -0400

    create docable screen

--- src/Spann/Spann/ui/framework/dist/build.js
+++ src/Spann/Spann/ui/framework/dist/build.js
@@ -469,24 +469,22 @@ function Editor(parent, screen) {
     enableBasicAutocompletion: true
   });
 
-  editor.commands.addCommand({
-      name: "send",
-      bindKey: {win: "Enter", mac: "Enter"},
-      exec: function(editor) {
-        console.log("GO Go GO!!")
-        var data = "data = 123";
-        editor.setValue(data, data.length - 1);
-      }
-  });
-
   editor.setTheme("ace/theme/twilight");
 
-  Object.defineProperty(object.model, 'e', {
-    get: function () {
-      return editor;
+  // Object.defineProperty(object.model, 'editor', {
+  //   get: function () {
+  //     return editor;
+  //   }
+  // });
+
+  Object.defineProperty(object.model, 'value', {
+    get: function() {
+      return editor.getValue();
+    },
+    set: function(value) {
+      editor.setValue(value);
     }
   });
-
 
   Object.defineProperty(object.model, 'mode', {
     set: function(value) {
@@ -1213,6 +1211,81 @@ function Dialog(parent, screen) {
 }
 
 $ui.addExtension('Dialog', Dialog);
+
+var DockLocations = {
+  TOP: "top",
+  BOTTOM: "bottom",
+  LEFT: "left",
+  RIGHT: "right"
+};
+
+$ui.addStyleExtension('DockLocations', DockLocations);
+
+function DockScreen(parent, screen) {
+  var object = $ui.BaseHolder(parent, screen);
+  object.component.addClass('ui-dock-screen');
+
+  // var modal = $ui.create(object.component);
+  // modal.addClass('modal');
+
+  var content = $ui.create(object.component);
+  content.addClass('modal-content');
+  object.addContainer(content);
+
+  if(object._private.alignVertical === undefined) {
+    object._private.alignVertical = $ui.DockLocations.TOP;
+  }
+  Object.defineProperty(object.model, "alignVertical", {
+    get: function() {
+      return object._private.alignVertical;
+    },
+    set: function(value) {
+      if(value !== object._private.alignVertical) {
+        object._private.alignVertical = value;
+        switch(object._private.alignVertical) {
+          case $ui.DockLocations.TOP:
+            return object.component.replaceClass("align-bottom", "align-top");
+          case $ui.DockLocations.BOTTOM:
+            return object.component.replaceClass("align-top", "align-bottom");
+          default:
+            return object.component.replaceClass("align-bottom", "align-top");
+        }
+      }
+    }
+  });
+
+  if(object._private.alignHorizontal === undefined) {
+    object._private.alignHorizontal = $ui.DockLocations.LEFT;
+  }
+  Object.defineProperty(object.model, "alignHorizontal", {
+    get: function() {
+      return object._private.alignHorizontal;
+    },
+    set: function(value) {
+      if(value !== object._private.alignHorizontal) {
+        object._private.alignHorizontal = value;
+        switch(object._private.alignVertical) {
+          case $ui.DockLocations.LEFT:
+            return object.component.replaceClass("align-right", "align-left");
+          case $ui.DockLocations.RIGHT:
+            return object.component.replaceClass("align-left", "align-right");
+          default:
+            return object.component.replaceClass("align-right", "align-left");
+        }
+      }
+    }
+  });
+
+  window.onclick = function(event) {
+    if (event.target === object.component) {
+        $ui.pop();
+    }
+  }
+
+  return object;
+}
+
+$ui.addExtension('DockScreen', DockScreen);
 
 function Flow(parent, screen) {
   var object = $ui.BaseHolder(parent, screen);
