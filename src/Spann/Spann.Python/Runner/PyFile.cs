9a14afac3d4947aac5a56c88c9703646f362d2f5
Christopher Stokes <c.stokes99@gmail.com>
2016-10-14 18:03:13 -0400

    Add basis for python project and running.

---
+++ src/Spann/Spann.Python/Runner/PyFile.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spann.PythonTools.Runner
+{
+    public class PyFile : IPyLocation
+    {
+        #region Public Field(s).
+        public string Location { get; private set; }
+        public string Name { get; private set; }
+        #endregion
+
+        #region Private Field(s).
+        private FileInfo file;
+        #endregion
+
+        #region  Public Constructor(s).
+        public PyFile(string location, string name, string data = null)
+        {
+            this.Location = location;
+            this.Name = name;
+            if (!Directory.Exists(location))
+            {
+                Directory.CreateDirectory(location);
+            }
+            string path = PyTools.CreatePath(location, name);
+            file = new FileInfo(path);
+            using (var fs = file.Create())
+            {
+                if (data != null)
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes(data);
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+        }
+        #endregion
+
+        #region Public Member(s).
+        public bool CanDelete
+        {
+            get
+            {
+                return file != null && file.Exists;
+            }
+        }
+        public void Delete()
+        {
+            if (file != null && file.Exists)
+            {
+                file.Delete();
+            }
+        }
+
+        public bool DoesExist
+        {
+            get
+            {
+                return file != null && file.Exists;
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/Spann/Spann.Python/Runner/PyProject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spann.PythonTools.Runner
+{
+    public class PyProject : IPyLocation
+    {
+        #region Public Field(s).
+        public string Location { get; private set; }
+        public string Name { get; private set; }
+        #endregion
+
+        #region Private Field(s).
+        private DirectoryInfo directory;
+        private List<PyFile> files;
+        #endregion
+
+        #region Public Constructor(s).
+        public PyProject(string location, string name)
+        {
+            this.Location = location;
+            this.Name = name;
+            string path = PyTools.CreatePath(location, name);
+            directory = Directory.CreateDirectory(path);
+            files = new List<PyFile>();
+        }
+        #endregion
+
+        #region Public Member(s).
+        public bool CanDelete
+        {
+            get
+            {
+                if (directory != null && directory.Exists)
+                {
+                    return files.Count(file => file.DoesExist()) == 0;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public void Delete()
+        {
+            if(directory != null && directory.Exists)
+            {
+                directory.Delete();
+            }
+        }
+
+        public bool DoesExist()
+        {
+            return directory != null && directory.Exists;
+        }
+
+        public void AddFile(PyFile file)
+        {
+            files.Add(file);
+        }
+        #endregion
+    }
+}
