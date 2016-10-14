c8b670f920ad7e51e02d4b60842228a824a5a7e0
Christopher Stokes <c.stokes99@gmail.com>
2016-10-14 18:03:13 -0400

    Add basis for python project and running.

---
+++ src/Spann/Spann.Python/Runner/IPyLocation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spann.PythonTools.Runner
+{
+    public interface IPyLocation
+    {
+        bool CanDelete { get; }
+        void Delete();
+
+        bool DoesExist { get; }
+    }
+}
