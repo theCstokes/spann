477d17e6dc48ffd1ee196efc10c6836c63cd4841
stokescd <stokescd@mcmaster.ca>
2016-10-14 18:03:13 -0400

    Add basis for python project and running.

---
+++ src/Spann/Spann.Python/Runner/PyTools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spann.PythonTools.Runner
+{
+    class PyTools
+    {
+        public static string CreatePath(string location, string name)
+        {
+            return Path.Combine(location, name);
+        }
+
+        public static string CreatePath(string location, params string[] additons)
+        {
+            string path = location;
+            return additons.Aggregate(path, (result, item) => Path.Combine(path, item));
+        }
+    }
+}
