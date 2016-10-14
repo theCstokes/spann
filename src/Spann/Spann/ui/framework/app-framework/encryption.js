e7e2428cc4a3c34443b2cb1771d82c645c426b72
stokescd <stokescd@mcmaster.ca>
2016-10-14 15:39:35 -0400

    Add rehash function to encription.

--- src/Spann/Spann/ui/framework/app-framework/encryption.js
+++ src/Spann/Spann/ui/framework/app-framework/encryption.js
@@ -2,13 +2,25 @@ define(['SHA512'], function(sha512) {
   /**
    * Return a salted hash and the salt
    */
-  function saltyHash(string) {
+  function saltyHash(value) {
     var buf = new Uint8Array(128);
     var saltBytes = window.crypto.getRandomValues(buf);
 
     var salt = saltBytes.reduce(function(str, item) { return str + item }, "");
 
-    var hash = string + salt;
+    var hash = value + salt;
+    for (var i=0; i<1000; i++) {
+      hash = sha512.sha512(hash + salt);
+    }
+
+    return {hash: hash, salt: salt};
+  }
+
+  /**
+   * Return a salted hash and the salt
+   */
+  function rehash(value, salt) {
+    var hash = value + salt;
     for (var i=0; i<1000; i++) {
       hash = sha512.sha512(hash + salt);
     }
@@ -17,6 +29,7 @@ define(['SHA512'], function(sha512) {
   }
 
   return {
-    saltyHash: saltyHash
+    saltyHash: saltyHash,
+    rehash: rehash
   }
 });
