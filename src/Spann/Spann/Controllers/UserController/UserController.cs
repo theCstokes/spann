81438f061200f83c1eb7633220cb2a7b49849363
Christopher Stokes <c.stokes99@gmail.com>
2016-10-14 15:40:41 -0400

    Update to server to allow login.

--- src/Spann/Spann/Controllers/UserController/UserController.cs
+++ src/Spann/Spann/Controllers/UserController/UserController.cs
@@ -1,6 +1,8 @@
-﻿using Spann.Core.JsonTools;
+﻿using Spann.Core.Headers;
+using Spann.Core.JsonTools;
 using Spann.DomainModel.Users;
 using Spann.RepositoryModel;
+using Spann.ResponseBuilders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +19,38 @@ namespace Spann.Controllers
         [Route("User")]
         public IHttpActionResult GetAllUsers()
         {
-            List<UserDM> users = RC.UserManager.PullAll();
+
+            List<UserDM> users = null;
+            if (Request.Headers.Contains("SearchHeader"))
+            {
+                SearchRequestHeader header = Request.RetrieveHeader<SearchRequestHeader>();
+                users = RC.UserManager.PullAll(u => u.Username == header.Items[0].Value);
+            }
+            else
+            {
+                users = RC.UserManager.PullAll();
+            }
             return Ok(JsonUtils.SerializeObject(users));
+        }
+
+        [HttpPost]
+        [Route("User/Login")]
+        public IHttpActionResult Login([FromBody] UserDM user)
+        {
+            var foundUser = RC.UserManager.Pull(u => u.Name == user.Name);
+            if(foundUser != null)
+            {
+                return ResponseUtils.CreateResponse(HttpStatusCode.OK, foundUser.Password == user.Password);
+            }
+            return ResponseUtils.CreateResponse(HttpStatusCode.OK, false);
+        }
+
+        [HttpPost]
+        [Route("User/Create")]
+        public IHttpActionResult Create([FromBody] UserDM user)
+        {
+            RC.UserManager.Commit(CommitTypeEnum.ADD, user);
+            return ResponseUtils.CreateResponse(HttpStatusCode.OK, user);
         }
     }
 }
