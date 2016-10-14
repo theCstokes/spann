91ff6508c5742422b037d9d932608ad37183b3f5
stokescd <stokescd@mcmaster.ca>
2016-10-14 18:03:13 -0400

    Add basis for python project and running.

---
+++ src/Spann/Spann.Python/Runner/PyRunner.cs
@@ -0,0 +1,24 @@
+﻿using Spann.Core.DomainModel.Python;
+using Spann.DomainModel.Users;
+using Spann.PythonTools.Runner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spann.PythonTools
+{
+    class PyRunner
+    {
+        public static void Launch(UserDM owner, PythonProjectDM projectDM)
+        {
+            var project = PyServerManager.CreateProject(owner.Username, projectDM.Name);
+            projectDM.Files.ForEach(fileDM =>
+            {
+                var file = PyServerManager.CreateFile(owner.Username, fileDM.Name, fileDM.SourceCode);
+                project.AddFile(file);
+            });
+        }
+    }
+}
