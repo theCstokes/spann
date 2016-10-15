8298eee476ae17251b642fdfdb3254f404cfdf35
stokescd <stokescd@mcmaster.ca>
2016-10-15 10:33:02 -0400

    Fix name mistake.

--- src/Spann/Spann.Python/Console/PyConsoleManager.cs
+++ src/Spann/Spann.Python/Console/PyConsoleManager.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Spann.PythonTools
+namespace Spann.PythonTools.Console
 {
-    public class PyConsoleManager.Console
+    public class PyConsoleManager
     {
         #region Private Field(s).
         private static Dictionary<Guid, PyConsole> consoles;
