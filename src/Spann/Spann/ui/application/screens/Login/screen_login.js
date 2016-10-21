acbfc83016b77b597b7917ea3efdf32dd4eae465
hoodav <hoodav@mcmaster.ca>
2016-10-20 21:50:38 -0700

    change login background to new login picture

--- src/Spann/Spann/ui/application/screens/Login/screen_login.js
+++ src/Spann/Spann/ui/application/screens/Login/screen_login.js
@@ -127,7 +127,7 @@ define([
 
     screen.registerEvent('show', function () {
       console.log(this);
-      screen.uiObject.model.backgroundImage = '/ui/application/resources/login3.jpg';
+      screen.uiObject.model.backgroundImage = '/ui/application/resources/login4.jpg';
 
       var components = screen.model;
 
