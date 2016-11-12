cd20501a69761b550d281e2be685e2c5204317b3
stokescd <stokescd@mcmaster.ca>
2016-11-12 13:59:27 -0500

    remove extra ui folder

--- src/Spann/Spann/ui/libs/JJS/boot.js
+++ src/Spann/Spann/ui/libs/JJS/boot.js
@@ -1,13 +1,13 @@
 function ui_boot(callback) {
   console.log('Booting UI Framework...');
   loadScripts([
-    "./framework/ui-framework/js/$ui.js",
-    "./framework/ui-framework/js/$builder.js",
-    "./framework/app-framework/$T.js",
-    "./framework/app-framework/$data.js",
-    "./framework/$utils.js",
-    "./framework/dist/build.js",
-    "./framework/libs/ace/src-min-noconflict/ace.js"
+    "./libs/JJS/ui-framework/js/$ui.js",
+    "./libs/JJS/ui-framework/js/$builder.js",
+    "./libs/JJS/app-framework/$T.js",
+    "./libs/JJS/app-framework/$data.js",
+    "./libs/JJS/$utils.js",
+    "./libs/JJS/dist/build.js",
+    "./libs/JJS/libs/ace/src-min-noconflict/ace.js"
   ], function() {
       console.log('UI Framework Started.');
       callback();
