2501ccbbbb1d8bf0653dc940199b83e78d6b9a37
hoodav <hoodav@mcmaster.ca>
2016-11-19 12:42:46 -0500

    Fixed C# comment build warnings

--- src/Spann/Spann/Controllers/RequestExtensions.cs
+++ src/Spann/Spann/Controllers/RequestExtensions.cs
@@ -11,8 +11,17 @@ using System.Web.Http.Controllers;
 
 namespace Spann.Controllers
 {
+    /// <summary>
+    /// Request Extensions class
+    /// </summary>
     public static class RequestExtensions
     {
+        /// <summary>
+        /// Retrieve Header
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Request"></param>
+        /// <returns></returns>
         public static T RetrieveHeader<T> (this HttpRequestMessage Request) where T : ICustomHeaderType
         {
             string name = ((HeaderTypeAttribute)typeof(T).GetCustomAttributes(typeof(HeaderTypeAttribute), false).First()).Name;
